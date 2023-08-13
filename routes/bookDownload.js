a9b261656a35e90a170ad0bcd07e54d367108f68
Mikele Corleone <sellmar38@gmail.com>
2023-08-14 00:44:21 +1000

    v1.3

--- routes/bookDownload.js
+++ routes/bookDownload.js
@@ -8,7 +8,7 @@ router.get("/api/books/:id/download", (req, res) => {
 	const { id } = req.params;
 	const index = books.findIndex((book) => book.id === id);
 	if (books[index].fileBook) {
-    const origFileName = books[index].fileBook.split("-")[1];
+    const origFileName = books[index].fileBook.split("-")[(books[index].fileBook).split("-").length - 1];
     res.download(path.join(__dirname, '../', books[index].fileBook), origFileName);
 	} else {
 		res.status(404);
