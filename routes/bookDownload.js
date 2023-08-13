fae983aaf24a8c98f5b8b2947389d5723babbbf0
Mikele Corleone <sellmar38@gmail.com>
2023-08-14 00:16:44 +1000

    v1.2

--- routes/bookDownload.js
+++ routes/bookDownload.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bookStore = require("../store/books");
 
 const router = express.Router();
@@ -7,7 +8,8 @@ router.get("/api/books/:id/download", (req, res) => {
 	const { id } = req.params;
 	const index = books.findIndex((book) => book.id === id);
 	if (books[index].fileBook) {
-		res.json({ fileBook: books[index].fileBook });
+    const origFileName = books[index].fileBook.split("-")[1];
+    res.download(path.join(__dirname, '../', books[index].fileBook), origFileName);
 	} else {
 		res.status(404);
 		res.json("Code: 404");
